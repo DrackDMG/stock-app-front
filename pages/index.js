c7a419ec265a9b2757576028a7132fb52fb34e83
DrackDMG <92767963+DrackDMG@users.noreply.github.com>
2022-07-06 13:21:10 -0500

    add var de entorno a vercel

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div >
-      <h1> hola mundo</h1>
+      <h1> hola mundo </h1>
       <Link href='/productos/create'> Crear producto</Link>
     </div>
   )
